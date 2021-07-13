a42f15bed3f4c4ba2199e17648bda468bf611284
pratamabayu <pratama.bayu@hotmail.com>
2021-07-13 19:41:17 +0700

    Delete section-videos-as-aside.php

--- app/public/wp-content/themes/kampusku/template-parts/section/section-videos-as-aside.php
+++
@@ -1,9 +0,0 @@
-<div class="margin-large margin-remove-horizontal margin-remove-top">
-    <div class="flex align-items-center justify-content-between">
-        <div>
-            <h4>Koleksi video</h4>
-        </div>
-    </div>
-    <ul class="list list-style-none list-divider">
-    </ul>
-</div>
