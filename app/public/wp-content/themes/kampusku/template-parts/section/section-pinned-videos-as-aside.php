ada842022257aa68772f47d56eab01e83f18ae48
pratamabayu <pratama.bayu@hotmail.com>
2021-08-30 14:07:11 +0700

    Update

--- app/public/wp-content/themes/kampusku/template-parts/section/section-pinned-videos-as-aside.php
+++ app/public/wp-content/themes/kampusku/template-parts/section/section-pinned-videos-as-aside.php
@@ -27,9 +27,10 @@ $the_query = new WP_Query($query_args);
 
                     <?php if ($video_kind == 'youtube'): ?>
                         <div class="video-container">
-                            <iframe width="560" height="315" title="<?= get_the_title(); ?>" src="<?= $video_url; ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                            <iframe width="560" height="315" title="YouTube video player" src="<?= $video_url; ?>?rel=0&modestbranding=1&autohide=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
                             </iframe>
                         </div>
+                        <p><strong><?= get_the_title(); ?></strong></p>
                     <?php endif; ?>
                 </li>
             <?php endwhile; ?>
